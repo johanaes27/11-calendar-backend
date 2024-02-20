977ac59b21fa968870b5390cf6ed69b8f5bb0de5
Johana <johanaes2709@gmail.com>
2024-02-20 12:38:07 -0500

    Path * agregado

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.use( express.json() );
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+app.get('*', (req, res) => {
+    res.sendFile(__dirname  + '/public/index.html');
+})
+
 // TODO CRUD: Eventos
 
 
